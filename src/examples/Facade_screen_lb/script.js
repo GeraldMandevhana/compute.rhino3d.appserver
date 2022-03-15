749f116bdb68b2295d7c133b52c9f258376c75d6
GeraldMandevhana <gerald.mandevhana@students.iaac.net>
2022-03-15 11:11:08 +0200

    Updates to css, GH and html scripts

--- src/examples/Facade_screen_lb/script.js
+++ src/examples/Facade_screen_lb/script.js
@@ -14,6 +14,27 @@ const data = {
   definition: 'Facade_screen_lb.gh',
   inputs: getInputs()
 }
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".range");
+  const bubble = wrap.querySelector(".bubble");
+
+  range.addEventListener("input", () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+
+  // Sorta magic numbers based on size of the native UI thumb
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
 
 // globals
 let rhino, doc
